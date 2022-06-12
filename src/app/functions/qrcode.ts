1b6d082cfadbba3a63583138cbb663faa3b5676c
Erik Kralj <erik@kralj.dev>
2022-06-12 22:11:50 +0200

    add payment QR code to PDF invoice

---
+++ src/app/functions/qrcode.ts
@@ -0,0 +1,32 @@
+import { CreateData } from "./create-pdf-invoice";
+
+function formatAmount(amount: number): string {
+  const amountStr = amount.toFixed(2).replace(".", "");
+  return amountStr.padStart(11, "0");
+}
+
+export function qrencode(data: CreateData): string {
+  const text = [
+    "UPNQR",
+    "",
+    "",
+    "",
+    "",
+    data.invoice.client.fullName,
+    data.invoice.client.address,
+    data.invoice.client.postalCode + " " + data.invoice.client.postalOffice,
+    formatAmount(data.invoice.grossAmount),
+    "",
+    "",
+    "OTHR",
+    "Invoice payment",
+    data.datePipe.transform(data.invoice.validTo, data.dateFormat) || "",
+    data.invoice.company.bankTRR.replace(/\s+/gi, "").trim(),
+    "00 " + data.invoice.accNo,
+    data.invoice.company.fullName,
+    data.invoice.company.address,
+    data.invoice.company.postalCode + " " + data.invoice.company.postalOffice,
+  ];
+  const textSum = String(text.reduce((a, b) => a + b.length, 0) + 19);
+  return text.concat([textSum, "\n"]).join("\n");
+}
